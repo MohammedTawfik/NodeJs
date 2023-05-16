0102cc77e524d7079205b0a5875362a176535974
MohammedTawfik <m.abdelraza2@gmail.com>
2023-05-16 11:25:19 +0300

    Configure MonogDB Initialize Connection when starting the app

--- MongoDB-Sample/util/database.js
+++ MongoDB-Sample/util/database.js
@@ -1,8 +1,31 @@
-const Sequelize = require('sequelize');
+const MongoClient = require('mongodb').MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db = null;
 
-module.exports = sequelize;
+async function DbConnect() {
+  try {
+    let url =
+      'mongodb+srv://admin:Windows.2000@cluster0.xwfguxf.mongodb.net/mysampledb?retryWrites=true&w=majority';
+    let _db = await MongoClient.connect(url);
+  } catch (e) {
+    return e;
+  }
+}
+
+async function Get() {
+  try {
+
+    if (db != null) {
+      return db;
+    } else {
+      db = await DbConnect();
+      return db;
+    }
+  } catch (e) {
+    return e;
+  }
+}
+
+
+exports.DbConnect = DbConnect;
+exports.GetDbConnection = Get;
